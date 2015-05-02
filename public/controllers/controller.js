f59fbabd5ae61e8b3fee78375b8811f594a46784
mingkai <mingkai@Zhaoyangs-Mac-mini.local>
2015-05-01 17:00:18 -0700

    completed MEAN without mongoose

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,8 +3,37 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
-	$http.get('/itemlist').success(function(response) {
-		console.log("I got the data I requested");
-		$scope.itemlist = response;
-	});
+	var refresh = function() {
+		$http.get('/itemlist').success(function(response) {
+			$scope.itemlist = response;
+		});
+	};
+
+	refresh();
+
+	$scope.addItem = function() {
+		console.log($scope.item);
+		$http.post('/itemlist', $scope.item).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.removeItem = function(id) {
+		$http.delete('/itemlist/' + id).success(function(response) {
+			refresh();
+		});
+	};
+
+	$scope.editItem = function(id) {
+		$http.get('/itemlist/' + id).success(function(response) {
+			$scope.item = response;
+		});
+	};
+
+	$scope.update = function() {
+		$http.put('/itemlist/' + $scope.item._id, $scope.item).success(function(response) {
+			refresh();
+		});
+	};
+
 }]);
--- server.js
+++ server.js
@@ -1,37 +1,71 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var itemdb = mongojs('itemlist', ['itemlist']);
+var bodyParser = require('body-parser');
+
 var port = 2000;
 
+// access static files (html, and angular controller in public)
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json())
 app.listen(port);
 
+// obtain mongodb data
 app.get('/itemlist', function(req, res) {
-	var item1 = {
-		title: "Concerto no.1",
-		author: "Bach",
-		price: 17.90,
-		stock: 5,
-		category: "classical"
-	};
+	itemdb.itemlist.find(function(err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+});
 
-	var item2 = {
-		title: "Concerto no.2",
-		author: "Bach",
-		price: 13.90,
-		stock: 10,
-		category: "classical"
-	};
+// add data
+app.post('/itemlist', function(req, res) {
+	console.log(req.body);
 
-	var item3 = {
-		title: "I will always love you",
-		author: "Celine Dion",
-		price: 19.90,
-		stock: 15,
-		category: "classical"
-	};
+	itemdb.itemlist.insert(req.body, function(err, doc) {
+		res.json(doc);
+	});
+});
 
-	var itemlist = [item1, item2, item3];
-	res.json(itemlist);
+// remove data
+app.delete('/itemlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(id);
+	itemdb.itemlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	})
+});
+
+// search data
+app.get('/itemlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(id);
+
+	itemdb.itemlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+// update data
+app.put('/itemlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(req.body.title);
+
+	itemdb.itemlist.findAndModify({
+		query: {_id: mongojs.ObjectId(id)},
+		update: { // getting data from request body
+				 $set: {title: req.body.title, 
+						author: req.body.author, 
+						price: req.body.price, 
+						stock: req.body.stock, 
+						category: req.body.category}
+				},
+		new: true}, 
+		function(err, doc) {
+			res.json(doc);
+		}
+	);
 });
 
 console.log("listening at port "+port);
