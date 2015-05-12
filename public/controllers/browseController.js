dac1f36660615f55b7508d8b94d572a87ed10a46
mkaichen <mingkaichen2009@gmail.com>
2015-05-12 20:25:22 +0000

    browseController fix

--- public/controllers/browseController.js
+++ public/controllers/browseController.js
@@ -5,15 +5,21 @@ var myApp = angular.module('myApp-browse', []);
 // BROWSE VIEW
 myApp.controller('browseCtrl', ['$scope', '$http', 'mySharedService', function($scope, $http, sharedService) {
 	var refresh = function() {
+		// use jquery to dynamically find and place new categories in html
+
 		$http.get('/itemlist').success(function(response) {
-			if (response) {
-				$scope.userMessage = 'Welcome '+response;
-				$scope.loginAccessMessage = 'log out'
-			} else {
-				$scope.userMessage = 'Please Sign in';
-				$scope.loginAccessMessage = 'sign in'
-			}
-		})
+			$scope.itemlist = response;
+		});
+
+		var signedIn = false;
+		var username = "Admin"
+		if (signedIn) {
+			$scope.userMessage = 'Welcome '+username;
+			$scope.loginAccessMessage = 'log out';
+		} else {
+			$scope.userMessage = 'Please Sign in';
+			$scope.loginAccessMessage = 'sign in';
+		}
 	};
 
 	refresh();
@@ -23,29 +29,8 @@ myApp.controller('browseCtrl', ['$scope', '$http', 'mySharedService', function($
 	}
 
 	$scope.searchItem = function() {
-		$scope.itemlist = [item1, item2, item3];
+		console.log($scope.criteria);
 	}
-
-	var item1 = {title: "Concerto no 1", 
-				author: "Bach", 
-				price: 10, 
-				stock: 20, 
-				category: "Classic",
-				taxable: false};
-
-	var item2 = {title: "Concerto no 2", 
-				author: "Bach", 
-				price: 11, 
-				stock: 22, 
-				category: "Classic",
-				taxable: false};
-
-	var item3 = {title: "Crazy in Love", 
-				author: "Beyonce", 
-				price: 12, 
-				stock: 30, 
-				category: "?",
-				taxable: true};
 }]);
 
 })();
