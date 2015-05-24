dce670d7180a3e16231713bab90cd965cf5922f6
mingkaic <mingkaic@sfu.ca>
2015-05-24 10:33:08 -0700

    changed styles

--- public/globalNav.js
+++ public/globalNav.js
@@ -1,16 +1,3 @@
-var checksNav = function() {
-	console.log($(document).find("login"))
-	if ($(document).find("login").length)
-		$("#globalheader").find("#userInfo").children().hide();
-
-	if ($(document).find("browse").length)
-		$("#globalheader").find("#userInfo").children().show();
-
-	if ($(document).find("manage").length)
-		$("#globalheader").find("#userInfo").children().show();
-};
-
 $(document).ready(function() {
-	checksNav();
-	$("nav").on("click", checksNav());
+	
 });
